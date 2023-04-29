db9d6103bed7b696bd9266e00a33e4c97aad7978
Hugo Tannús <htannus.dev@gmail.com>
2023-04-29 12:50:42 -0300

    Separates `dataSupplierService` in its own file

--- app/js/services.js
+++ app/js/services.js
@@ -1,4 +1,3 @@
-var dataSupplierService = angular.module("dataSupplierService", []);
 var sequenceMatcherService = angular.module("sequenceMatcherService", []);
 var sequenceDialogService = angular.module("sequenceDialogService", [
   "colors",
@@ -39,40 +38,6 @@ dataCollectionService.factory("DataCollection", [
             sequences
           );
         }, rate);
-      },
-    };
-  },
-]);
-
-dataSupplierService.factory("DataChunk", [
-  function ($interval) {
-    return {
-      weights: [],
-      getBuffer: function (bufferSize, weights) {
-        this.weights = weights;
-        var i;
-        var buffer = " ";
-        for (i = 0; i < bufferSize; i++) buffer += this.getChunk();
-        return buffer;
-      },
-      getChunk: function () {
-        return this.getBit() * 16 + this.getBit() * 4 + this.getBit() * 1 + " ";
-      },
-      getBit: function () {
-        return this.weight(Math.random());
-      },
-      weight: function (randomNumber) {
-        var range = this.weights[0];
-        var i = 1;
-
-        do {
-          if (randomNumber < range) return i - 1;
-
-          range += this.weights[i];
-          i++;
-        } while (i <= this.weights.length);
-
-        return i - 1;
       },
     };
   },
