e4fe07258e635291e8a6e8a3776b5e69cd39be33
Hugo Tannús <htannus.dev@gmail.com>
2023-04-29 18:50:18 -0300

    Reverte alterações para não quebrar testes.

--- app/directives/minion-rectangle.directive.js
+++ app/directives/minion-rectangle.directive.js
@@ -10,13 +10,7 @@
       restrict: "AE",
       replace: true,
       template: function () {
-        return '\
-          <div class="paddingHack"> \
-            <svg class="rect" viewBox = "0 0 100 100" style = "fill:{{sequence.color}}"> \
-              <rect x = "0" y = "0" width = "200" height = "200"  /> \
-            </svg> \
-          </div> \
-        ';
+        return '<div class="paddingHack"><svg class="rect" viewBox = "0 0 100 100" style = "fill:{{sequence.color}}"><rect x = "0" y = "0" width = "200" height = "200" /></svg></div>';
       },
     };
   }
