a6f0d9760ad281dc4a37e672215eeb124cc82eea
Hugo Tannús <htannus.dev@gmail.com>
2023-04-29 18:30:33 -0300

    Separate `minionRectangle` directive in its own file

---
+++ app/directives/minion-rectangle.directive.js
@@ -0,0 +1,23 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('MinIONApp')
+    .directive("minionRectangle", minionRectangle);
+
+  function minionRectangle() {
+    return {
+      restrict: "AE",
+      replace: true,
+      template: function () {
+        return '\
+          <div class="paddingHack"> \
+            <svg class="rect" viewBox = "0 0 100 100" style = "fill:{{sequence.color}}"> \
+              <rect x = "0" y = "0" width = "200" height = "200"  /> \
+            </svg> \
+          </div> \
+        ';
+      },
+    };
+  }
+}());
--- app/js/directives.js
+++ app/js/directives.js
@@ -120,16 +120,3 @@ MinIONApp.directive("barsChart", function ($parse) {
   };
   return directiveDefinitionObject;
 });
-
-// MinIONApp.directive("minionRectangle", [
-//   "colorService",
-//   function (colorService) {
-//     return {
-//       restrict: "AE",
-//       replace: true,
-//       template: function (elem, attrs) {
-//         return '<div class="paddingHack"><svg class="rect" viewBox="0 0 100 100" style="fill:{{sequence.color}}"><rect x="0" y="0" width="200" height="200" /></svg></div>';
-//       },
-//     };
-//   },
-// ]);
